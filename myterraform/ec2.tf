514fe4e96ba6890a92feb23be98658bb78fbac9a
Chakri <chakravarthishroff24@gmail.com>
2023-05-06 07:14:47 +0530

    terraform practice

--- myterraform/ec2.tf
+++ myterraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "roboec2" {
   #ami = data.aws_ami.roboimage.id
   ami = "ami-067a8829f9ae24c1c"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   tags = {
     Name = "helloworld"
   }
